78dee8faa18735279c4be6d1c97ac4f78abd02e5
Harsh Maurya <mauryah535@gmail.com>
2025-08-19 09:55:34 -0400

    Refactor game logic to implement snake and ladder mechanics; enhance play functionality and turn management

--- client/index.js
+++ client/index.js
@@ -1,31 +1,42 @@
-const socket = io('ws://192.168.1.36:5000')
+const maxNumberOfPawns = 12;
 
-const turnEle = document.getElementById("turn")
-turnEle.innerHTML = ' '
-const diceValueEle = document.getElementById("dice")
-diceValueEle.innerHTML = ''
+const turnEle = document.getElementById("turn");
+turnEle.innerHTML = " ";
+const diceValueEle = document.getElementById("dice");
+diceValueEle.innerHTML = "";
 
-const userName = prompt("enter your name")
-const pwanValue = prompt("enter number 0: blue, 1:yellow, 2:red, 3: green")
-const colorList = ["blue", "yellow", "red", "green"]
-const pwanColor = colorList[pwanValue]
-socket.on('info', (msg) => {
-  console.log(msg)
-  console.log(`Name: ${userName}, ID : ${socket.id}`)
-})
-socket.on('game', ({ diceValue, clients, turn }) => {
-  console.log(clients, turn, socket.id)
+const userName = prompt("enter your name");
+const pawnValue =
+  prompt("enter number 0: blue, 1:yellow, 2:red, 3: green") || 0;
+
+const totalPawnImageEleList = [];
+for (let i = 0; i < maxNumberOfPawns; i++) {
+  const imageElement = new Image(`img${i}`);
+  imageElement.src = `./pawns/${i}.png`;
+  totalPawnImageEleList.push(imageElement);
+}
+const pawnEle = totalPawnImageEleList[pawnValue];
+const pawnImageEleList = totalPawnImageEleList.filter((e) => e !== pawnEle);
+
+const socket = io("ws://192.168.1.36:5000");
+
+socket.on("info", (msg) => {
+  console.log(msg);
+  console.log(`Name: ${userName}, ID: ${socket.id} `);
+});
+socket.on("game", ({ diceValue, clients, turn }) => {
+  console.log(clients, turn, socket.id);
   if (turn === socket.id) {
-    turnEle.innerHTML = 'Your Turn'
+    turnEle.innerHTML = "Your Turn";
   } else {
-    const c = clients.filter((e) => e.socketId == turn)
-    turnEle.innerHTML = `Turn : ${c[0].name}`
+    const c = clients.filter((e) => e.socketId == turn);
+    turnEle.innerHTML = `Turn: ${c[0].name} `;
   }
-  diceValueEle.innerHTML = diceValue ? diceValue : ''
-  draw(clients)
-})
+  diceValueEle.innerHTML = diceValue ? diceValue : "";
+  draw(clients);
+});
 
-socket.emit('info', userName)
+socket.emit("info", userName);
 
 const path = {
   1: { x: 0, y: 9 },
@@ -128,29 +139,29 @@ const path = {
   98: { x: 2, y: 0 },
   99: { x: 1, y: 0 },
   100: { x: 0, y: 0 },
-}
+};
 
-const canvasSize = 600
-const blockSize = canvasSize / 10
+const canvasSize = 600;
+const blockSize = canvasSize / 10;
 
 const webpImage = new Image();
-webpImage.src = '../map.png'; // Replace with your WebP image path
-const canvasEle = document.getElementById("canvas")
-canvasEle.height = canvasSize
-canvasEle.width = canvasSize
-const ctx = canvasEle.getContext("2d")
+webpImage.src = "../map.png"; // Replace with your WebP image path
+const canvasEle = document.getElementById("canvas");
+canvasEle.height = canvasSize;
+canvasEle.width = canvasSize;
+const ctx = canvasEle.getContext("2d");
 
 webpImage.onload = () => {
   ctx.drawImage(webpImage, 0, 0, canvasSize, canvasSize); // Example with custom position and size
 };
 
-const playBtnEle = document.getElementById("play")
+const playBtnEle = document.getElementById("play");
 // playBtnEle.disabled = true
-playBtnEle.style.backgroundColor = "#fcc"
-playBtnEle.style.color = "#000"
-playBtnEle.addEventListener('click', () => {
-  socket.emit("play", "")
-})
+playBtnEle.style.backgroundColor = "#fcc";
+playBtnEle.style.color = "#000";
+playBtnEle.addEventListener("click", () => {
+  socket.emit("play", "");
+});
 
 const drawCircle = (x, y, r, fillColor) => {
   ctx.beginPath();
@@ -160,7 +171,7 @@ const drawCircle = (x, y, r, fillColor) => {
   ctx.lineWidth = 4;
   ctx.strokeStyle = "white";
   ctx.stroke();
-}
+};
 
 const drawLine = (x1, y1, x2, y2) => {
   ctx.beginPath();
@@ -168,30 +179,38 @@ const drawLine = (x1, y1, x2, y2) => {
   ctx.lineTo(x2, y2);
   ctx.lineWidth = 1;
   ctx.stroke();
-}
+};
 
-const drawPawn = (pathNum, color) => {
-  drawCircle(blockSize / 2 + blockSize * path[pathNum].x, blockSize / 2 + blockSize * path[pathNum].y, blockSize / 2 - blockSize / 6, color)
-}
+const drawPawn = (img, pathNum) => {
+  ctx.drawImage(
+    img,
+    blockSize / 6 + blockSize * path[pathNum].x,
+    blockSize / 6 + blockSize * path[pathNum].y,
+    blockSize - blockSize / 3,
+    blockSize - blockSize / 3
+  );
+};
 
 // y axis line draw
 for (let i = 1; i < 10; i++) {
-  drawLine(blockSize * i, 0, blockSize * i, canvasSize)
+  drawLine(blockSize * i, 0, blockSize * i, canvasSize);
 }
 for (let i = 1; i < 10; i++) {
-  drawLine(0, blockSize * i, canvasSize, blockSize * i)
+  drawLine(0, blockSize * i, canvasSize, blockSize * i);
 }
 
 const draw = (clients) => {
-
   // clear screen
-  ctx.clearRect(0, 0, canvasSize, canvasSize)
+  ctx.clearRect(0, 0, canvasSize, canvasSize);
   ctx.drawImage(webpImage, 0, 0, canvasSize, canvasSize); // Example with custom position and size
   // draw pawn
   clients.forEach((e) => {
-    drawPawn(e.position, "cyan")
+    drawPawn(
+      pawnImageEleList[Math.floor(Math.random() * pawnImageEleList.length)],
+      e.position
+    );
     if (e.socketId === socket.id) {
-      drawPawn(e.position, pwanColor)
+      drawPawn(pawnEle, e.position);
     }
-  })
-}
+  });
+};
